38a4f9fb5b016e7f4045bcd292c9714ad8de9ef5
abt236 <Welcome1>
2017-01-03 16:44:07 -0500

    Adding an action creator called createCourse

---
+++ src/actions/courseActions.js
@@ -0,0 +1,6 @@
+export function createCourse(course){
+  // return {type: 'CREATE_COURSE', course: course};
+  //In ES6 because the left hand side matches the right hande we can use
+  //course only instead of course:course
+  return {type: 'CREATE_COURSE', course};
+}
