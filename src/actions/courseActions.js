0063abc47ac911a5f06d7d419c8119bdc307b8d6
abt236 <Welcome1>
2017-01-05 10:05:54 -0500

    Created a Thunk for loading courses, added the action creator and made adjustment to the reducer to handle the action

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,8 +1,27 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course){
+// export function createCourse(course){
+//   // return {type: 'CREATE_COURSE', course: course};
+//   //In ES6 because the left hand side matches the right hande we can use
+//   //course only instead of course:course
+//   return {type: types.CREATE_COURSE, course};
+// }
+
+export function loadCoursesSuccess(courses){
   // return {type: 'CREATE_COURSE', course: course};
   //In ES6 because the left hand side matches the right hande we can use
   //course only instead of course:course
-  return {type: types.CREATE_COURSE, course};
+  return {type: types.LOAD_COURSES_SUCCESS, courses};
+}
+
+export function loadCourses(){
+  return function(dispatch){
+    return courseApi.getAllCourses().then(
+      courses => dispatch(loadCoursesSuccess(courses))
+     ).catch(
+       //curly braces are needed for the fat arrow when using throw.
+       //something to do with
+      error => {throw(error);});
+  };
 }
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,17 +2,20 @@ import * as types from '../actions/actionTypes';
 
 export default function courseReducer(state = [], action){
   switch (action.type) {
-    case types.CREATE_COURSE:
-        // The spread operator creates an inline spread of the state array
-        // i.e. the ... takes the state array passed in and creates an inline
-        // spread here in this new array. To this, we then add the Object.assign
-        // and add the action.course that is passed in.
+    // case types.CREATE_COURSE:
+    //     // The spread operator creates an inline spread of the state array
+    //     // i.e. the ... takes the state array passed in and creates an inline
+    //     // spread here in this new array. To this, we then add the Object.assign
+    //     // and add the action.course that is passed in.
+    //
+    //     //This syntax from ES6 is very useful to create a new array and add a new
+    //     //value to it.
 
-        //This syntax from ES6 is very useful to create a new array and add a new
-        //value to it.
-        return [...state,
-            Object.assign({},action.course)
-        ];
+    //     return [...state,
+    //         Object.assign({},action.course)
+    //     ];
+    case types.LOAD_COURSES_SUCCESS:
+        return action.courses;       
     default:
         return state;
   }
